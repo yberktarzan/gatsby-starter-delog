cc222031f8f37ba9d3d74f2e37ca5fc549df69dd
yberktarzan <43090597+yberktarzan@users.noreply.github.com>
2021-10-20 00:04:06 +0300

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,6 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <Link to="https://discord.gg/9CE6CuqH3p">Discord Sunucusu</Link>
     <Link to="/contact">İletişim</Link>
     <ThemeChanger/>
   </nav>
