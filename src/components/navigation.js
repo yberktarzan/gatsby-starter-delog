ed187d3d81731f0911a58deb454506f9cb4f3ffd
yberktarzan <43090597+yberktarzan@users.noreply.github.com>
2021-10-19 23:36:14 +0300

    navigation and index update
    add discord link and translate

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,8 +4,8 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
-    <Link to="/contact">Discord</Link>
+    <Link to="https://discord.gg/9CE6CuqH3p">Discord Sunucusu</Link>
+    <Link to="/contact">İletişim</Link>
     <ThemeChanger/>
   </nav>
   
